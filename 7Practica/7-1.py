283d94b0d177119889ef98f171c21d4b3f70e98c
JuanALoraC <juanalorags@gmail.com>
2021-11-10 14:20:41 +0100

    7 dos

--- 7Practica/7-1.py
+++ 7Practica/7-1.py
@@ -4,15 +4,33 @@ JaLora
 # === Imports ===
 import os
 # === Comandos ===
+continua=0
+diccionario={}
 os.system("clear") # Comando para limpiar la terminal
-print("==============") # Esto es simplemente por estetica
-nombre=input("Escribe tu nombre: ") # Pedimos el nombre
-apellido= input("Escribe tus apellido: ") # Pedimos el apellido
-dni=input("Escribe tu DNI: ") # Pedimos el DNI
-print("============")
 
-log=(nombre[0:2]) + (apellido[0:3]) + (dni[-3:]) # Aqui lo que hacemos es concatenar los valores que le indicamos siendo 
+while continua != "xxx":
+    print("==============") # Esto es simplemente por estetica
+
+    nombre=input("Escribe tu nombre: ") # Pedimos el nombre
+    apellido= input("Escribe tus apellido: ") # Pedimos el apellido
+    dni=input("Escribe tu DNI: ") # Pedimos el DNI
+    contraseña=input("Escribe una contraseña: ")
+
+    print("============")
+
+    log=(nombre[0:2]) + (apellido[0:3]) + (dni[-3:]) # Aqui lo que hacemos es concatenar los valores que le indicamos siendo 
                                                  # el primer numero entre corchetes la posicion inicial y el ultimo 
                                                  # la posicion final.
-print(f"Tu login es el siguiente: {log}") # Mostramos por pantalla la variable que hemos almacenado con los valores anteriores
+
+    continua=input(f"Añade usuario: {log} enter para seguir: ")
+    diccionario[log] = contraseña
+
+    check=input(f"añadido usuario {log} salir o continua  (xxx-salir): ")
+    if check != "xxx":
+        pass
+    else:
+        break
+
+print(diccionario)
+
 # Final de Archivo
